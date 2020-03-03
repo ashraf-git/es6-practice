a8492d19497599023b7a044dd211f12b2c1d7cb1
ashraf-git <aliashraf.hena@gmail.com>
2020-03-03 08:13:50 +0600

    Let, const, array declared with const, object declared with const

---
+++ letconst.js
@@ -0,0 +1,17 @@
+const hubby = "omar sani";
+//hubby = "salman";
+console.log(hubby);
+const numbers = [34, 45, 54, 54];
+numbers[3] = 12;
+numbers.push(32);
+// numbers = ["kamal", "sabuj", "rana", "simul"];
+// console.log(numbers);
+// const nayaok = {name: "kamal",age: 34, phone: 433424};
+let patientName = "Kamal";
+patientName = "rahima";
+console.log(patientName);
+let sum = 0;
+for( var i = 0; i < 10; i++){
+    sum = sum + i;
+}
+console.log(i);
