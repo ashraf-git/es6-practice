adbcde4d101c525f7dc53a59365e2bdf51f08fdb
ashraf-git <aliashraf.hena@gmail.com>
2020-03-03 17:35:11 +0600

    Class, constructor, create object from class

---
+++ class.js
@@ -0,0 +1,12 @@
+class student{
+    constructor(sID, sName) {
+        this.id = sID;
+        this.name = sName;
+        this.school = "kalimunnesa";
+
+    }
+}
+const student1 = new student(23, "kaaml");
+const student2 = new student(34, "rana");
+const student3 = new student(35, "ashraf");
+console.log(student3);
